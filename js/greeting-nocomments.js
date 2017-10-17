4042a392940a64ac12b4082f241182069cace17d
Jake Buytenhuys <P15185285@LEC-ADMIN.dmu.ac.uk>
2017-10-17 15:29:15 +0100

    new let

--- js/greeting-nocomments.js
+++ js/greeting-nocomments.js
@@ -2,9 +2,11 @@
   let myName = document.getElementById("my-name"); 
   let userName = document.getElementById("user-name"); 
   let getName = document.getElementById("get-name");
+  let headingName = document.getElementById("heading-name")
   
   function PerformGreeting() {
     myName.innerHTML = userName.value;
+	headingName.innerHTML = userName.value;
     event.preventDefault();
     return false;
   }
